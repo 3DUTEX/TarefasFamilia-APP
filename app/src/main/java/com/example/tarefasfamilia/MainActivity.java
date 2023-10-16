4282512ed419042262be6f93b20336e14f032875
t3xdev <t3xdev@gmail.com>
2023-10-16 14:17:44 -0300

    associacao feita

--- app/src/main/java/com/example/tarefasfamilia/MainActivity.java
+++ app/src/main/java/com/example/tarefasfamilia/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.tarefasfamilia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinnerResponsavel;
+    RadioButton radioPendente, radioRealizados, radioTodos;
+
+    Button buttonNovaTarefa;
+
+    ListView listViewTarefas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        associa();
+
+        
+    }
+
+    private void associa() {
+        spinnerResponsavel = findViewById(R.id.spinnerResponsavel);
+        radioPendente = findViewById(R.id.radioPendente);
+        radioRealizados = findViewById(R.id.radioRealizados);
+        radioTodos = findViewById(R.id.radioTodos);
+        buttonNovaTarefa = findViewById(R.id.buttonNovaTarefa);
+        listViewTarefas = findViewById(R.id.listViewTarefas);
     }
 }
